3e15a5bb04570049e23ed64463cd90927f449bbc
xrr2016 <xiaoranran1993@163.com>
2017-04-27 23:15:19 +0800

    :bulb:

--- user-dashboard/src/index.js
+++ user-dashboard/src/index.js
@@ -1,8 +1,10 @@
-import dva from 'dva';
-import './index.css';
+import dva from 'dva'
+import './index.css'
 
 // 1. Initialize
-const app = dva();
+const app = dva()
+
+app.model(require('./models/users'))
 
 // 2. Plugins
 // app.use({});
@@ -11,7 +13,7 @@ const app = dva();
 // app.model(require('./models/example'));
 
 // 4. Router
-app.router(require('./router'));
+app.router(require('./router'))
 
 // 5. Start
-app.start('#root');
+app.start('#root')
--- user-dashboard/src/router.js
+++ user-dashboard/src/router.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import { Router, Route } from 'dva/router';
-import IndexPage from './routes/IndexPage';
+import React from 'react'
+import { Router, Route } from 'dva/router'
+import IndexPage from './routes/IndexPage'
 
-function RouterConfig({ history }) {
+import Users from './routes/Users.js'
+
+function RouterConfig ({ history }) {
   return (
     <Router history={history}>
-      <Route path="/" component={IndexPage} />
+      <Route path='/' component={IndexPage} />
+      <Route path='/users' component={Users} />
     </Router>
-  );
+  )
 }
 
-export default RouterConfig;
+export default RouterConfig
