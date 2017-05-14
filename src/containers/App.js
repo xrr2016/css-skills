c831f22074bf5c5f7e1d7f14fef2696fed3a8f66
xrr2016 <xiaoranran1993@163.com>
2017-05-14 22:36:28 +0800

    :zap:

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import logo from '../assets/logo.svg'
+// import logo from '../assets/logo.svg'
 import '../styles/App.css'
 
 class App extends Component {
     render() {
         return (
-            <h1>Hello <img src={logo} alt=""/></h1> 
+            <h1>Hello</h1> 
         )
     }
 }
